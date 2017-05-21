1fca273188b261340f6f5335b32a5acd5ed4a0f3
SimonLucas <simonmarklucas@gmail.com>
2017-05-21 22:53:29 +0100

    Ready to add a new rope game

---
+++ src/ropegame/RopeGameState.java
@@ -0,0 +1,12 @@
+package ropegame;
+
+/**
+ * Created by Simon Lucas on 21/05/2017.
+ *
+ *  Ready for an interesting and fast to compute game
+ *  involving and agent using rope to swing from place
+ *  to place - may then end up with PTSP-like variants etc.
+ *
+ */
+public class RopeGameState {
+}
