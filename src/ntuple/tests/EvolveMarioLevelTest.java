70ce226e6b21e20b3d6f1f47324d8c0882bca01d
SimonLucas <simonmarklucas@gmail.com>
2018-02-26 18:33:03 +0000

    Minor changes

--- src/ntuple/tests/EvolveMarioLevelTest.java
+++ src/ntuple/tests/EvolveMarioLevelTest.java
@@ -27,7 +27,7 @@ import static levelgen.MarioReader.*;
 public class EvolveMarioLevelTest implements EvolutionListener {
 
     static int imageWidth = 30, imageHeight = 20;
-    static int filterWidth = 3, filterHeight = 3;
+    static int filterWidth = 4, filterHeight = 4;
     static int stride = 1;
 
     static boolean useInitialSeed = true;
@@ -43,6 +43,7 @@ public class EvolveMarioLevelTest implements EvolutionListener {
         Mutator mutator = new Mutator(null);
         mutator.flipAtLeastOneValue = true;
         mutator.pointProb = 2;
+
         mutator.setSwap(true);
         simpleRMHC.setMutator(mutator);
 
@@ -132,7 +133,7 @@ public class EvolveMarioLevelTest implements EvolutionListener {
         int[] solution = ea.runTrial(evaluator, nEvals);
 
         // can set entire solution to the most likely individual
-        // solution = setAll(solution, 2);
+        solution = setAll(solution, 2);
 
         double fitness = evaluator.evaluate(solution);
         String label = String.format("Fitness: %.6f", fitness);
