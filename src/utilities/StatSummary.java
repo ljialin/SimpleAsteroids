4ba8ea94715cf5b959e9067fb0a4ff379c33856e
SimonLucas <simonmarklucas@gmail.com>
2018-02-27 12:51:09 +0000

    Revert "Merge branch 'master' of https://github.com/ljialin/SimpleAsteroids"
    This reverts commit 8be8d8be72e5a798048b396efc82b397f44d71b9, reversing
    changes made to 172ee551783996dba7d087c01db1b1d795006d7e.

--- src/utilities/StatSummary.java
+++ src/utilities/StatSummary.java
@@ -95,11 +95,6 @@ public class StatSummary {
         return mean;
     }
 
-    public double sum() {
-        if (strict && n < 1) throw new RuntimeException(strictMessage);
-        return sum;
-    }
-
     // returns the sum of the squares of the differences
     //  between the mean and the ith values
     public double sumSquareDiff() {
