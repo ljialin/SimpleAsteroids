8ac02caf398ba29d14d90e6c2feaa22ddede5276
Simon Lucas <simonmarklucas@gmail.com>
2018-03-02 15:52:09 +0000

    In process of adding a Mouse listener to enable interactive exploration

---
+++ src/ntuple/visuals/ConvMouseListener.java
@@ -0,0 +1,13 @@
+package ntuple.visuals;
+
+/*
+     Purpose of this class is to operate as follows:
+ */
+
+import java.awt.event.MouseAdapter;
+
+public class ConvMouseListener extends MouseAdapter {
+
+    
+
+}
