78a63e6cc3bbea56b8f521d1a3d5ad41eec0a3a1
SimonLucas <simonmarklucas@gmail.com>
2018-03-16 23:25:22 +0000

    Added another test point to hyperopt.TestSolutionPlanetWars

--- src/hyperopt/TestSolutionPlanetWars.java
+++ src/hyperopt/TestSolutionPlanetWars.java
@@ -8,11 +8,14 @@ public class TestSolutionPlanetWars {
     public static void main(String[] args) {
 
         // use this code to re-rest a particular point in the search space
-        GameState.includeBuffersInScore = false;
+        GameState.includeBuffersInScore = true;
+
+        EvoAgentSearchSpace.tickBudget = 2000;
+
         int[] solution = {2, 1, 0, 0, 2};
         // int[] solution = {1, 0, 0, 0, 4};
         solution = new int[]{3, 1, 1, 0, 3};
-        solution = new int[]{3, 1, 1, 0, 3};
+        solution = new int[]{3, 1, 1, 0, 2};
         System.out.println(new EvoAgentSearchSpace().report(solution));
         int nChecks = 100;
         ElapsedTimer timer = new ElapsedTimer();
