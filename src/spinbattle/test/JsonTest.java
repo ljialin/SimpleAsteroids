b19c75acd2340cf287cdafb519fc61d44b1e5c5c
SimonLucas <simonmarklucas@gmail.com>
2018-05-09 16:13:30 +0100

    Added a JSON test method

---
+++ src/spinbattle/test/JsonTest.java
@@ -0,0 +1,21 @@
+package spinbattle.test;
+
+import com.google.gson.Gson;
+import spinbattle.core.SpinGameState;
+import spinbattle.params.SpinBattleParams;
+
+public class JsonTest {
+    public static void main(String[] args) {
+        SpinBattleParams params = new SpinBattleParams();
+        params.maxTicks = 100;
+        SpinGameState gameState = new SpinGameState().setParams(params).setPlanets();
+
+        Gson gson = new Gson();
+        System.out.println("Serialising...");
+        String json = gson.toJson(gameState);
+        System.out.println(json);
+
+        System.out.println();
+        System.out.println(json.length());
+    }
+}
