a3ea9f0d5ae206c82e854622a63d423b80a1f957
Simon Lucas <simonmarklucas@gmail.com>
2018-05-17 14:08:10 +0100

    Updated the game runner p1 v p2 code

--- src/spinbattle/test/GeneralGameRunnerTest.java
+++ src/spinbattle/test/GeneralGameRunnerTest.java
@@ -14,7 +14,7 @@ public class GeneralGameRunnerTest {
     public static void main(String[] args) {
         ElapsedTimer t = new ElapsedTimer();
 
-        int maxTicks = 1000;
+        int maxTicks = 2000;
         GameRunner runner = new GameRunner().setLength(maxTicks);
         SimplePlayerInterface p1 = new RandomAgent();
         SimplePlayerInterface p2 = new RandomAgent();
@@ -24,7 +24,6 @@ public class GeneralGameRunnerTest {
         evoAgent.setSequenceLength(100);
         runner.setPlayers(evoAgent, p2);
         SpinGameStateFactory factory = new SpinGameStateFactory();
-        factory.params.getRandom().setSeed(50);
         factory.params.maxTicks = maxTicks;
         runner.setGameFactory(factory);
         // runner.ga
@@ -33,11 +32,15 @@ public class GeneralGameRunnerTest {
         // runner.playGames(20);
 
         // todo: while this one works
-        for (int i=0; i<3; i++) {
+        for (int i=0; i<100; i++) {
+            factory.params.getRandom().setSeed(i);
             EvoAgent evoAgent2 = evoAgentFactory.getAgent();
-            evoAgent.setSequenceLength(100);
-            runner.setPlayersWithoutReset(evoAgent2, p2);
+            evoAgent2.setSequenceLength(50);
+            EvoAgent evoAgent3 = evoAgentFactory.getAgent().setSequenceLength(200);
+            runner.setPlayersWithoutReset(evoAgent2, evoAgent3);
             runner.playGame();
+            System.out.println(runner.p1Wins + "\t " + runner.p2Wins);
+            System.out.println();
         }
 
         runner.plotGameScores();
