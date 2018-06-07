f0621c17c20bca8409d2fc3732bf88e55dfe3d0e
Simon Lucas <simonmarklucas@gmail.com>
2018-06-07 09:11:45 +0100

    Playing with ForwardDiscountTest

--- src/spinbattle/test/ForwardDiscountTest.java
+++ src/spinbattle/test/ForwardDiscountTest.java
@@ -2,7 +2,9 @@ package spinbattle.test;
 
 
     /*
-    A simple test class to play with the effects of varying a discount factor
+    A simple test class to play with the effects of varying a discount factor;
+
+    Not working properly yet...
      */
 
 public class ForwardDiscountTest {
@@ -13,7 +15,7 @@ public class ForwardDiscountTest {
         int score = 0;
         int scorePerStep = 10;
         double discount = 1.0;
-        double discountFactor = 2;
+        double discountFactor = 1.0;
 
         double tot = 0;
         double denom = 1.0;
@@ -23,7 +25,10 @@ public class ForwardDiscountTest {
 
         for (int i=0; i<nSteps; i++) {
             score += scorePerStep;
-            value = score*score;
+
+            // make the value a function of the score (could just be the raw score)
+            // value = score*score;
+            value = Math.sqrt(score);
             discount *= discountFactor;
             tot += discount * value;
             denom += discount;
