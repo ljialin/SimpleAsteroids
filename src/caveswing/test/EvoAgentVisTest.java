9abdb0436f2b4baf17a6bebb0049a2812a56e281
SImon Lucas <simonmarklucas@gmail.com>
2019-01-08 13:35:19 +0000

    Update EvoAgentVisTest.java

--- src/caveswing/test/EvoAgentVisTest.java
+++ src/caveswing/test/EvoAgentVisTest.java
@@ -27,7 +27,7 @@ public class EvoAgentVisTest {
 
     static boolean useFalseModel = false;
 
-    static int frameDelay = 500;
+    static int frameDelay = 50;
 
     public static void main(String[] args) throws Exception {
         int nEvals = 20;
