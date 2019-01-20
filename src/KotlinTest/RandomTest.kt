ee31ff17439eec0eca440a6312b9e13581dd7b47
SImon Lucas <simonmarklucas@gmail.com>
2019-01-20 23:27:33 +0000

    Create RandomTest.kt

---
+++ src/KotlinTest/RandomTest.kt
@@ -0,0 +1,12 @@
+package KotlinTest
+
+import kotlin.random.*
+import kotlin.math.*
+
+
+fun main(args: Array<String>) {
+    for (i in 0 until 10) {
+// still not supported: weird
+        // println(Random.nextInt(10))
+    }
+}
